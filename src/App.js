8c4b984a2a3661edca567de5ab0f43b8ef952262
Deborah <debbyuzuegbu@gmail.com>
2023-02-04 00:01:54 +0100

    added contact,experience

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./Components/About";
+import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
@@ -12,8 +13,8 @@ function App() {
       <Home />
       <About />
       <Portfolio />
-
       <Experience />
+      <Contact />
       <SocialLinks />
     </div>
   );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -36,7 +37,7 @@ const NavBar = () => {
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={Link} smooth duration={500}>{link}</Link>
           </li>
         ))}
       </ul>
