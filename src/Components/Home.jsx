ad81152c56665bfaef12b0b3c8bc652b20e4eda4
Deborah <debbyuzuegbu@gmail.com>
2023-02-03 01:26:12 +0100

    added style to the home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,23 +4,31 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className="flex h-screen w-full bg-gradient-to-b from-black to-gray-800">
-      <div>
-        <div>
-          <h2>I am a Frontend Engineer</h2>
-          <p>
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Frontend Engineer</h2>
+          <p className="text-gray-500 ">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Distinctio, fuga reiciendis modi, aperiam consequuntur aliquam
             eveniet tempora unde repellendus quaerat nam obcaecati fugit
             mollitia ex voluptatem totam amet quod? Vel?
           </p>
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <MdOutlineKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
         </div>
-        <button>
-            Portfolio
-            <span>
-            <MdOutlineKeyboardArrowRight />
-            </span>
-        </button>
+        <div>
+          <img className="rounded-2xl mx-auto w-1/4 md:w-full" src={heroImage} alt="my profile"/>
+        </div>
       </div>
     </div>
   );
