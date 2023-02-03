7f1208bd09c8e7ed1e68a03f00509a884de404cf
Deborah <debbyuzuegbu@gmail.com>
2023-02-03 04:49:35 +0100

    added sociallinks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
+import SocialLinks from "./Components/SocialLinks";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <SocialLinks />
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,28 +6,34 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-md mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Frontend Engineer</h2>
-          <p className="text-gray-500 ">
+          <h2 className="text-4xl font-bold sm:text-7xl text-white">
+            I am a Frontend Engineer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Distinctio, fuga reiciendis modi, aperiam consequuntur aliquam
             eveniet tempora unde repellendus quaerat nam obcaecati fugit
             mollitia ex voluptatem totam amet quod? Vel?
           </p>
           <div>
-            <button>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
-              <span>
-                <MdOutlineKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
               </span>
             </button>
           </div>
         </div>
         <div>
-          <img className="rounded-2xl mx-auto w-1/4 md:w-full" src={heroImage} alt="my profile"/>
+          <img
+            className="rounded-2xl mx-auto w-1/3"
+            src={heroImage}
+            alt="my profile"
+          />
         </div>
       </div>
     </div>
