d09f8b161d3c8568f128861764e9a593046b4e59
Deborah <debbyuzuegbu@gmail.com>
2023-02-04 21:37:36 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
 import Portfolio from "./Components/Portfolio";
 import SocialLinks from "./Components/SocialLinks";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Routes>
       <NavBar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Experience />
-      <Contact />
-      <SocialLinks />
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="portfolio" element={<Portfolio />} />
+      <Route path="experience" element={<Experience />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="sociallinks" element={<SocialLinks />} />
+      </Routes>
     </div>
   );
 }
