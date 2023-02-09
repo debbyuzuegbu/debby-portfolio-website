3534fde19d82227ad5b5662cd932114dd7e50354
Deborah <debbyuzuegbu@gmail.com>
2023-02-09 11:28:54 +0100

    changed portfolio website

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -59,7 +59,7 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-3 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-3 md:grid-cols-3 gap-8 md:px-12 sm:px-0">
           {portfolios.map(({ id, src, demolink, codelink }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
