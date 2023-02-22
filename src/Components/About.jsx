272b21edd99f5fa30d9c74034448f119a189f75a
Deborah <debbyuzuegbu@gmail.com>
2023-02-22 02:54:50 +0100

    added about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,19 +7,20 @@ const About = () => {
       className="w-full bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="mt-20 md:mt-36 md:mb-36">
+        <div className="mt-20 md:mt-24 md:mb-36">
           <p className="mt-24 text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
-          <p className="text-base mt-5">
+          <p className="text-xl mt-5">
             Deborah is a frontend Engineer and a certified Financial modelling
             valuation analyst with over two years experience that cuts across
-            technology and finance. However She has a keen interest in
-            technology and how she can use it to solve problems. Deborah enjoys
-            coding.
+            technology and finance. She has a keen interest in technology and
+            how she can use it to solve problems. Deborah enjoys coding. Able to
+            work effectively with team, she has great commununication
+            skills and very passionate about aesthestics and responsive websites.
           </p>
           <br />
-          <p className="text-base">
+          <p className="text-xl">
             My stack/technologies:
             <ul style={{ listStyle: "none" }}>
               <li>CSS/Sass</li>
